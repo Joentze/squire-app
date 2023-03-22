28e08504efe88918afedf520c825126b45089c39
Joen Tan <joentze01@gmail.com>
2023-03-23 00:11:37 +0800

    feat: added write file from object

--- squire-backend/engine-builder/builder.py
+++ squire-backend/engine-builder/builder.py
@@ -1,12 +1,30 @@
 """Module to build pickle file"""
+import pickle
 from uuid import uuid4
 import firebase_admin
 from firebase_admin import credentials, storage
 
-cred = credentials.Certificate(
+try:
+    cred = credentials.Certificate(
     """/Users/tanjoen/Documents/squire-app/squire-backend/engine-builder/credentials/keys.json""")
-firebase_admin.initialize_app(
+    firebase_admin.initialize_app(
     cred, {"storageBucket": "squire-backend.appspot.com"})
+except Exception as error_message:
+    firebase_admin.initialize_app()
+
+
+def upload_object_as_file(obj:object, filename:str)->None:
+    "upload object as file in pickled bytes"
+    pickle_bytes = pickle.dumps(obj)
+    token = uuid4()
+    bucket = storage.bucket()
+    blob = bucket.blob(filename)
+    blob.metadata = {"firebaseStorageDownloadTokens": token}
+    blob.upload_from_string(pickle_bytes)
+    blob.make_public()
+    gcs_storage_url = blob.public_url
+    return gcs_storage_url
+
 
 
 def upload_file(file_path: str) -> str:
@@ -22,5 +40,5 @@ def upload_file(file_path: str) -> str:
 
 
 if __name__ == "__main__":
-    url = upload_file("MinHashForest.pickle")
-    print(url)
+    obj = {"hello":"world"}
+    url = upload_object_as_file(obj, "binfile")
