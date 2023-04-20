d9fb60a3c93a79a8cf1a8c8940a52ce236827c91
Joen Tan <joentze01@gmail.com>
2023-04-20 18:28:03 +0800

    feat: added dashboard layout

--- squire-frontend/src/components/nav/NavFrame.tsx
+++ squire-frontend/src/components/nav/NavFrame.tsx
@@ -2,12 +2,18 @@ import { Outlet } from "react-router-dom";
 
 const NavFrame = () => {
   return (
-    <>
-      <p>Navigation Bar</p>
-      <div id="detail">
-        <Outlet />
+    <div className="w-full h-screen flex flex-col">
+      <div className="w-full flex h-16 border border-b-2"></div>
+      <div className="grid grid-cols-9 w-full flex-grow">
+        <div className="">
+          <div className="w-20 h-full border border-r-2 flex flex-col gap-2"></div>
+        </div>
+        <div id="detail" className="col-span-7 py-12 w-full h-full">
+          <Outlet />
+        </div>
+        <div></div>
       </div>
-    </>
+    </div>
   );
 };
 export default NavFrame;
--- squire-frontend/src/pages/dashboard/DashboardPage.tsx
+++ squire-frontend/src/pages/dashboard/DashboardPage.tsx
@@ -1,5 +1,21 @@
+import { ActionIcon } from "@mantine/core";
+import { IconAdjustments, IconPlus } from "@tabler/icons-react";
 import React from "react";
 const DashboardPage = () => {
-  return <>Dashboard</>;
+  return (
+    <div className="w-full h-full pb-16">
+      <div className="w-full flex flex-row">
+        <p className="text-gray-700 text-3xl font-bold flex-grow">Dashboard</p>
+        <ActionIcon className="mt-2">
+          <IconPlus size="2.125rem" />
+        </ActionIcon>
+      </div>
+      <p className="mt-2 text-gray-500">
+        {" "}
+        Supercharge your business by building recommendation engines.
+      </p>
+      <div className="w-full bg-gray-200 h-full mt-4"></div>
+    </div>
+  );
 };
 export default DashboardPage;
