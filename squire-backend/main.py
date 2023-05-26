5fdadf2a3055427f502c7167f3f39e0db98aa7a3
Joen Tan <joentze01@gmail.com>
2023-05-26 20:46:53 +0800

    feat: added key imports in python

--- squire-backend/main.py
+++ squire-backend/main.py
@@ -1,2 +1,15 @@
 """main file"""
-from access_keys import *
+import os
+
+# ================================================
+# ====================FOR KEYS====================
+try:
+    from access_keys import SUPABASE_PWD, SUPABASE_URL, OPENAI_API_KEY
+except ImportError:
+    SUPABASE_URL = os.environ["SUPABASE_URL"]
+    SUPABASE_PWD = os.environ["SUPABASE_PWD"]
+    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+    print("Error: Put file 'access_keys.py' in ./squire-backend")
+
+# ====================FOR KEYS====================
+# ================================================
