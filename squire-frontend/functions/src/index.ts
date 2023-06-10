59af87ddfbb5a0d7556dd49aa833b91e03e5d841
Joen Tan <joentze01@gmail.com>
2023-06-10 22:48:36 +0900

    refactor: removed unused code

--- squire-frontend/functions/src/index.ts
+++ squire-frontend/functions/src/index.ts
@@ -21,13 +21,6 @@ exports.onChunkCreated = functions.firestore
   .document("/chunks/{chunk}")
   .onCreate(async (snap, context) => {
     const { docs } = snap.data();
-    const jobs: Promise<any>[] = [];
-    for (let doc of docs) {
-      const job = new Promise(async () => {
-        return await createEmbedding(doc);
-      });
-      jobs.push(job);
-    }
     return await Promise.all(
       docs.map(async (doc: string) => {
         fetch("https://api.openai.com/v1/embeddings", {
@@ -53,49 +46,3 @@ exports.onChunkCreated = functions.firestore
       })
       .catch((error) => console.error(error));
   });
-
-const createEmbedding = async (doc: string) => {
-  try {
-    const response = await fetch("https://api.openai.com/v1/embeddings", {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${OPEN_AI_KEY.value()}`,
-      },
-      method: "POST",
-      body: JSON.stringify({
-        input: doc,
-        model: "text-embedding-ada-002",
-      }),
-    });
-
-    const { data } = await response.json();
-    const embedding = data[0].embedding;
-    return embedding;
-  } catch (e) {
-    console.log("there is an error");
-    throw new Error(e as string);
-  }
-};
-// const createEmbedding = async (
-//   document: string,
-//   token: string
-// ): Promise<any> => {
-//   try {
-//     console.log(document);
-//     const response = await axios.post(
-//       "https://api.openai.com/v1/embeddings",
-//       { input: document, model: "text-embedding-ada-002" },
-//       {
-//         headers: {
-//           "Content-Type": "application/json",
-//           Authorization: `Bearer ${token}`,
-//         },
-//       }
-//     );
-//     // console.log("response:", response);
-//     return response.data;
-//   } catch (e) {
-//     console.log("there is an error");
-//     throw new Error(e as string);
-//   }
-// };
