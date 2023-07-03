81aa8be2f90f724aac9a26482559c4d54be9d193
Joen Tan <joentze01@gmail.com>
2023-07-03 16:05:56 +0800

    feat: added private route component

---
+++ squire-frontend/src/route/PrivateRoute.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../firebase/auth/AuthContextWrapper";
+
+interface IProtectedRoute {
+  children: React.ReactNode | React.ReactNode[];
+}
+
+const ProtectedRoute: React.FC<IProtectedRoute> = ({ children }) => {
+  const auth = useAuth();
+  return (
+    <div className="">
+      {auth !== undefined ? <>{children}</> : <Navigate to={"/home"} />}
+    </div>
+  );
+};
+
+export default ProtectedRoute;
