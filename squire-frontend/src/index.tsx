b6db99b78698723a07b459c404009e9e8325c587
germaaine <gergoh.2022@smu.edu.sg>
2023-07-13 13:31:44 +0800

    feat: added signup page

--- squire-frontend/src/index.tsx
+++ squire-frontend/src/index.tsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
     element: <LandingPage />,
   },
   { path: "/login", element: <LoginPage /> },
-  { path: "/register", element: <SignUpPage /> },
+  { path: "login/register", element: <SignUpPage /> },
 
   {
     path: "*",
