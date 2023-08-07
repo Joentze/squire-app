2d38118a689158ebc82165d91e86e9ede63744c6
Joen Tan <joentze01@gmail.com>
2023-08-07 11:32:39 +0800

    feat: added emulators for firebase

--- squire-frontend/src/firebase/base.ts
+++ squire-frontend/src/firebase/base.ts
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,5 +24,11 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore();
+const functions = getFunctions(app);
+if (window.location.hostname.includes("localhost")) {
+  connectFirestoreEmulator(db, "127.0.0.1", 8001);
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  connectFunctionsEmulator(functions, "127.0.0.1", 5001);
+}
 
 export { auth, db, analytics };
